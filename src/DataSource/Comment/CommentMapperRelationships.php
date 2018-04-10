c5d13584d5ef875a068ac17c174e8e72c34ba229
Paul M. Jones <pmjones88@gmail.com>
2018-04-10 14:57:08 -0500

    track on changes to MapperRelationships

--- src/DataSource/Comment/CommentMapperRelationships.php
+++ src/DataSource/Comment/CommentMapperRelationships.php
@@ -10,9 +10,9 @@ class CommentMapperRelationships extends MapperRelationships
 {
     protected function define()
     {
-        $this->manyToOneByReference('commentable', 'related_type')
-            ->to('page', PageMapper::CLASS, ['related_id' => 'page_id'])
-            ->to('post', PostMapper::CLASS, ['related_id' => 'post_id'])
-            ->to('video', VideoMapper::CLASS, ['related_id' => 'video_id']);
+        $this->manyToOneVariant('commentable', 'related_type')
+            ->type('page', PageMapper::CLASS, ['related_id' => 'page_id'])
+            ->type('post', PostMapper::CLASS, ['related_id' => 'post_id'])
+            ->type('video', VideoMapper::CLASS, ['related_id' => 'video_id']);
     }
 }
